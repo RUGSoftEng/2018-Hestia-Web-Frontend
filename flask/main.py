2be826e1d35f2175cc1b4e202151d6f181193926
andrewlalis <andrewlalisofficial@gmail.com>
2018-03-13 14:47:25 +0100

    Renamed hello function.

--- flask/main.py
+++ flask/main.py
@@ -7,19 +7,18 @@ def main():
     return render_template('index.html')
 
 @app.route('/request', methods=['POST'])
-def hello():
+def apiRequestHandler():
     json = request.get_json()
     url = json["query"]
     method = json["method"]
     payload = json["payload"]
-    print(payload["required_info"]["name"])
     return routeRequest(method, url, payload)
 
 def routeRequest(method, query, payload):
 	switcher = {
 		'GET': requests.get(query, verify=False).text,
 		'POST': requests.post(query, verify=False, json=payload).text,
-		'PUT': requests.put(query, verify=False).text,
+		'PUT': requests.put(query, verify=False, json=payload).text,
 		'DELETE': requests.delete(query, verify=False).text,
 	}
 	return switcher.get(method, 'Invalid REST method');
