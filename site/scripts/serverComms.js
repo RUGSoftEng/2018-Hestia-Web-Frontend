618e1bf52114951f81e62313b3b648b090847db5
andrewlalis <andrewlalisofficial@gmail.com>
2018-04-02 10:44:59 +0200

    Moved some functions to eventHandler script, renamed functions.js to serverComms.js

--- site/scripts/serverComms.js
+++ site/scripts/serverComms.js
@@ -1,6 +1,6 @@
 /**
  * @fileOverview
- * @name functions.js
+ * @name serverComms.js
  * The file contains all the javascript needed to communicate with the Flask and
  * the remote Hestia controller. It needs refactoring and rearchitecturing.
  */
@@ -315,39 +315,6 @@ function viewDeviceActivators(deviceName, deviceId, activators) {
 }
 
 /**
- * Is a wrapper for the function our toggles calls. We currently have only one
- * type of toggle so the name is bad.
- */
-function onToggleInteracted() {
-    toggle(document.getElementById("serverAddress").value, this.name, this.id, this.checked);
-}
-
-/**
- * Is a wrapper for the function our sliders calls. We currently have only one
- * type of slider so the name is bad.
- */
-function onSliderInteracted() {
-    dimmer(document.getElementById("serverAddress").value, this.name, this.id, this.value / 100);
-}
-
-//When the user changes a slider's value.
-function onSliderInteracted(){
-    console.log("User changed slider: " + this.id + ", Current state: " + this.value);
-    updateDeviceActivator(globalServer(), SELECTED_DEVICE, this.id, 0.1);
-}
-
-window.onload = function() {
-    sendRequest(globalServer(), "/devices/", "GET", populateDevices);
-};
-/**
- * Is a wrapper for the function our button calls. We currently have only one
- * functionality for the buttons so the name is bad.
- */
-function onClickInteracted() {
-    postDevice(document.getElementById("serverAddress").value, document.getElementById("payload_input").value);
-}
-
-/**
  * Wrapper to update the re-populated the device list based on a promised json
  * object from the server.
  */
@@ -393,3 +360,7 @@ function postDevice(server, payload) {
     request.setRequestHeader("Content-type", "application/json");
     request.send(JSON.stringify(data));
 }
+
+window.onload = function() {
+    sendRequest(globalServer(), "/devices/", "GET", populateDevices);
+};
---
+++ site/scripts/ui/eventHandler.js
@@ -0,0 +1,35 @@
+/**
+ * @fileOverview
+ * @name eventHandler.js
+ * The file contains functions which are called when user interface events take place.
+ * It depends on functions provided by serverComms.js to send and receive data.
+
+/**
+ * Is a wrapper for the function our toggles calls. We currently have only one
+ * type of toggle so the name is bad.
+ */
+function onToggleInteracted() {
+    toggle(document.getElementById("serverAddress").value, this.name, this.id, this.checked);
+}
+
+/**
+ * Is a wrapper for the function our sliders calls. We currently have only one
+ * type of slider so the name is bad.
+ */
+function onSliderInteracted() {
+    dimmer(document.getElementById("serverAddress").value, this.name, this.id, this.value / 100);
+}
+
+//When the user changes a slider's value.
+function onSliderInteracted(){
+    console.log("User changed slider: " + this.id + ", Current state: " + this.value);
+    updateDeviceActivator(globalServer(), SELECTED_DEVICE, this.id, 0.1);
+}
+
+/**
+ * Is a wrapper for the function our button calls. We currently have only one
+ * functionality for the buttons so the name is bad.
+ */
+function onClickInteracted() {
+    postDevice(document.getElementById("serverAddress").value, document.getElementById("payload_input").value);
+}
