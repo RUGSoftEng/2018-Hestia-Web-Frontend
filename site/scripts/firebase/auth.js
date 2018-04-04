95d3e7283eb8270635e2498cd50372282a464055
andrewlalis <andrewlalisofficial@gmail.com>
2018-04-04 09:02:44 +0200

    Removed unnecessary functions from auth.js

--- site/scripts/firebase/auth.js
+++ site/scripts/firebase/auth.js
@@ -1,67 +1,43 @@
-function displaySignIn(){
-	document.getElementById("firebaseui-auth-container").setAttribute("display", "block");
-	var container = document.getElementById("container");
-	container.setAttribute("background-color", "black");
-	container.setAttribute("opacity", "0.25");
-}
-
-function hideSignIn(){
-	document.getElementById("firebaseui-auth-container").setAttribute("display", "none");
-	var container = document.getElementById("container");
-	container.setAttribute("background-color", "FFF");
-	container.setAttribute("opacity", "1");
-}
-
+//Initialization function, to be done at every page load.
 function initFirebase(){
-var config = {
-	apiKey: "AIzaSyAJzJqrgr5aX3IIaIVO0IYTNfYmStFwsI0",
-	authDomain: "hestia-web-2018.firebaseapp.com",
-	databaseURL: "https://hestia-web-2018.firebaseio.com",
-	projectId: "hestia-web-2018",
-	storageBucket: "hestia-web-2018.appspot.com",
-	messagingSenderId: "971236925335"
-};
-firebase.initializeApp(config);
+	var config = {
+		apiKey: "AIzaSyAJzJqrgr5aX3IIaIVO0IYTNfYmStFwsI0",
+		authDomain: "hestia-web-2018.firebaseapp.com",
+		databaseURL: "https://hestia-web-2018.firebaseio.com",
+		projectId: "hestia-web-2018",
+		storageBucket: "hestia-web-2018.appspot.com",
+		messagingSenderId: "971236925335"
+	};
+	firebase.initializeApp(config);
 }
 
-function showAuth(){
-// FirebaseUI config.
-var uiConfig = {
-	signInFlow: 'popup',
-	signInSuccessUrl: 'index.html',
-	signInOptions: [
-		// Leave the lines as is for the providers you want to offer your users.
-		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-		firebase.auth.EmailAuthProvider.PROVIDER_ID
-	],
-	// Terms of service url.
-	tosUrl: 'settings.html'
-};
-
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
-displaySignIn();
+//Promises to check login status, and either the user is logged in, or is not.
+function waitForLogin(){
+	return new Promise(function(resolve, reject){
+		firebase.auth().onAuthStateChanged(function(currentUser){
+			if (currentUser){//User has logged in successfully.
+				resolve(currentUser);
+			} else {//User is not logged in.
+				reject("Not logged in.");
+			}
+		});
+	});
 }
 
+//Signs out a logged in user.
 function signout(){
-firebase.auth().signOut().then(function(){
-	console.log("Signed out.");
-}, function(error){
-	console.log("Error while signing out.");
-});
+	firebase.auth().signOut().then(function(){
+		console.log("Signed out.");
+	}, function(error){
+		console.log("Error while signing out.");
+	});
 }
 
 initFirebase();
 
-//Check if the user is authenticated.
-firebase.auth().onAuthStateChanged(function(currentUser){
-if (currentUser){
-	//user is logged in.
-	console.log("User logged in:"+currentUser.displayName);
-} else {
-	//user is not logged in, prepare a sign-in popup.
-	showAuth();
-}
-});
+waitForLogin().then(user =>{
+	console.log("User is logged in: "+user.displayName);
+}).catch(err =>{
+	console.log(err);
+	window.location.replace("login.html");
+})
