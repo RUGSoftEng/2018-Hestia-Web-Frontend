ef3869659f369f733dfe9842dfe737929e5229dd
Troy Harrison <KuroNoPardalis@gmail.com>
2018-04-08 11:45:25 +0200

    Format code to be pep8 compliant

--- site/main.py
+++ site/main.py
@@ -5,34 +5,41 @@ from flask import Flask, render_template, request, jsonify
 import requests
 
 app = Flask(__name__,
-    static_url_path='',
-    static_folder='static')
+            static_url_path='',
+            static_folder='static')
 
 
 # Create endpoints
+
 @app.route('/')
 def main():
     return render_template('index.html')
+
 
 @app.route('/index.html')
 def index():
     return render_template('index.html')
 
+
 @app.route('/devices.html')
 def devices():
     return render_template('devices.html')
+
 
 @app.route('/server.html')
 def server():
     return render_template('server.html')
 
+
 @app.route('/settings.html')
 def settings():
     return render_template('settings.html')
 
+
 @app.route('/login.html')
 def login():
     return render_template('login.html')
+
 
 @app.route('/request', methods=['POST'])
 def apiRequestHandler():
@@ -44,10 +51,12 @@ def apiRequestHandler():
         payload = json["payload"]
     return routeRequest(method, url, payload)
 
+
 # Handle routing requests according to rest principles
+
 def routeRequest(method, query, payload):
     result = ""
-    print("Sending request to server:", method, ", ", query, ", ", payload);
+    print("Sending request to server:", method, ", ", query, ", ", payload)
     if (method == "GET"):
         result = requests.get(query, verify=False).text
     elif (method == "POST"):
