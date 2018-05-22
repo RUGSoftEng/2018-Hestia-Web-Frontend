89514be83e0a3db866ca4cc8415361678ef3f895
Elias Abdo <elias@RN-145-97-149-24.eduroam.rug.nl>
2018-05-22 12:46:42 +0200

    Reformatting Server.vue

--- src/components/Server.vue
+++ src/components/Server.vue
@@ -1,62 +1,59 @@
+require 'htmlbeautifier'
+beautiful = HtmlBeautifier.beautify(messy)
 <template lang="html">
   <div class="server">
-
-
-      <!-- The sui-breadcrumb -->
+    <!-- The sui-breadcrumb -->
     <sui-container class="ui raised segment breadcrumbs">
-      <sui-breadcrumb><h2>
-        <sui-breadcrumb-section link>
-          <router-link to="/">
-            Servers
-          </router-link>
-        </sui-breadcrumb-section>
-        <sui-breadcrumb-divider />
-        <sui-breadcrumb-section active>
+      <sui-breadcrumb>
+        <h2>
+          <sui-breadcrumb-section link>
+            <router-link to="/">
+              Servers
+            </router-link>
+          </sui-breadcrumb-section>
+          <sui-breadcrumb-divider />
+          <sui-breadcrumb-section active>
             {{this.$route.params.id}}
-        </sui-breadcrumb-section>
-      </h2></sui-breadcrumb>
+          </sui-breadcrumb-section>
+        </h2>
+      </sui-breadcrumb>
     </sui-container>
-
-
     <h2> {{server.IPAddress}} </h2>
-
     <DeviceGroup :server="this.server">
     </DeviceGroup>
   </div>
 </template>
-
 <script>
-import 'vue-range-slider/dist/vue-range-slider.css';
-import DeviceGroup from './DeviceGroup';
+  import 'vue-range-slider/dist/vue-range-slider.css';
+  import DeviceGroup from './DeviceGroup';
 
-export default {
-  props: [
+  export default {
+    props: [
     'auth', 'authenticated',
-  ],
-  components: {
-    DeviceGroup,
-  },
-  beforeMount() {
-    // eslint-disable-next-line
-    console.log(this.$route.params.id);
-    this.$store.dispatch('getServerDevices', { serverID: this.$route.params.id });
-  },
-  computed: {
-    server() {
-      return this.$store.state.currentServer;
+    ],
+    components: {
+      DeviceGroup,
     },
-  },
-  methods: {
-    presetChange(value) {
-      // eslint-disable-next-line
-      console.log(value);
-    },
-  },
-};
-</script>
-
-<style>
-.breadcrumbs {
+    beforeMount() {
+        // eslint-disable-next-line
+        console.log(this.$route.params.id);
+        this.$store.dispatch('getServerDevices', { serverID: this.$route.params.id });
+      },
+      computed: {
+        server() {
+          return this.$store.state.currentServer;
+        },
+      },
+      methods: {
+        presetChange(value) {
+          // eslint-disable-next-line
+          console.log(value);
+        },
+      },
+    };
+  </script>
+  <style>
+  .breadcrumbs {
     width: 70%;
     margin-bottom:25px !important;
     text-align: left;
